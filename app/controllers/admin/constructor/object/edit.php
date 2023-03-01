c4939af0e560e707fa6bd20f755c120691435f5d
Nik <system512@ya.ru>
2023-03-01 22:13:27 +0300

    Object Constructor: fix bugs

--- app/controllers/admin/constructor/object/edit.php
+++ app/controllers/admin/constructor/object/edit.php
@@ -2,5 +2,6 @@
 
 	$content = $APP->controller->run('admin/autoinclude', ['APP'=>$APP]);
 
+	$content['object'] = $_GET['object'];
 
 	$APP->template->file('admin/constructor/object/object_edit.html')->display($content);
--- app/controllers/admin/constructor/object/set.php
+++ app/controllers/admin/constructor/object/set.php
@@ -8,12 +8,21 @@
 	$collection	= urldecode($_GET['collection']);
 	$objectname	= urldecode($_GET['objectname']);
 
+	if (!$objectname)
+	{
+		http_response_code(400);
+		exit("Не указано имя объекта");
+	}
 
 	//~ echo 1; die;
 	if ($_POST['object'])
 	{
 		$object = (array) json_decode($_POST['object'], true);
-		print_r($object);
+		//~ print_r($object);
 		//~ var_dump($object);
-		echo ($APP->object->collection($collection)->set($objectname, $object));
+		if (!$APP->object->collection($collection)->set($objectname, $object))
+		{
+			http_response_code(400);
+			exit("Ошибка сохранения");
+		}
 	}
