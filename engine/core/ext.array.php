867cc2623f741e90bae77a3eaff1213d11efe975
Nik <system512@ya.ru>
2023-04-17 16:02:32 +0300

    -

--- engine/core/ext.array.php
+++ engine/core/ext.array.php
@@ -1,6 +1,17 @@
 <?php
 
+//TODO: DEPRICATED
 function array_keyding($array, $column)
+{
+	return array_combine( array_column((array) $array, $column), $array );
+}
+
+function array_keycolumn($column, $array)
+{
+	return array_combine( array_column((array) $array, $column), $array );
+}
+
+function array_key_column($column, $array)
 {
 	return array_combine( array_column((array) $array, $column), $array );
 }
