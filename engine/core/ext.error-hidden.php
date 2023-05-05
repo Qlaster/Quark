669d496c964bdb7c32f7905970464b820dfe456d
Nik <system512@ya.ru>
2023-05-05 11:29:55 +0300

    -

--- engine/core/ext.error-hidden.php
+++ engine/core/ext.error-hidden.php
@@ -1,7 +1,6 @@
 <?php
 
 //Hidden error "Undefined array key"
-
 //For others who come across this problem, here is a more complete way of doing this, which preserves the error message, but demotes it back to a notice.
 
 if (PHP_VERSION_ID > 80000)
@@ -10,15 +9,4 @@ if (PHP_VERSION_ID > 80000)
 	{
 		return str_starts_with($error, 'Undefined array key');
 	}, E_WARNING);
-
-    //~ set_error_handler(function($errno, $error)
-    //~ {
-        //~ if (!str_starts_with($error, 'Undefined array key'))
-        //~ {
-            //~ return false;  //default error handler.
-        //~ }else{
-            //~ trigger_error($error, E_USER_NOTICE);
-            //~ return true;
-        //~ }
-    //~ }, E_WARNING);
 }
