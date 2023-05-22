563112e41a40d7bcb8f4c2fdf25a1e0de7f7383a
Nik <system512@ya.ru>
2023-05-22 12:48:29 +0300

    -

--- app/controllers/admin/content/head/index.php
+++ app/controllers/admin/content/head/index.php
@@ -4,7 +4,7 @@
 	$content = $APP->controller->run('admin/autoinclude', ['APP'=>$APP]);
 
 	$content['title'] = 'Управление заголовками страниц';
-	$content['code']['head'] = $APP->page->meta->select(['urlmask'=>'','name'=>'headcode'])[0]['data'];
+	$content['code']['head'] = $APP->page->meta->select(['urlmask'=>'','name'=>'headcode'])[0]['data'] ?? '';
 
 	$APP->template->file('admin/content/page.head.html')->display($content);
 
