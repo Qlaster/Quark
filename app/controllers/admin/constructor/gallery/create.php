3eda3437872351919c7111f2bdc103899c4b03c9
Nik <system512@ya.ru>
2023-06-19 10:33:58 +0300

    Fix gallery

--- app/controllers/admin/constructor/gallery/create.php
+++ app/controllers/admin/constructor/gallery/create.php
@@ -5,7 +5,9 @@
 	$content = $APP->controller->run('admin/autoinclude', ['APP'=>$APP]);
 	error_reporting(E_ALL);
 
-	$APP->object->collection('gallery')->set($_GET['name'], null);
-
+	//Создадим галлерею, убедившись что ее не существует
+	if (! $APP->object->collection('gallery')->get($_GET['name']))
+		$APP->object->collection('gallery')->set($_GET['name'], null);
+	
 
 	header('Location: ../gallery');
