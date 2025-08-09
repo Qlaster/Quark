5a524958a730258977db603ded308bed069d0648
Nik <system512@ya.ru>
2025-08-09 11:56:06 +0300

    Delete session temp file

--- app/controllers/session/get.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-   var_dump( $APP->account->logged($_GET['token']) );
-
-	print_r($_SESSION);
--- app/controllers/session/set.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-    var_dump($APP->account->login('test', 'test'));
-
-    $_SESSION['=)'] = '=В';
