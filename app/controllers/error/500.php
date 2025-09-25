6ecc4f8fc80bb905f9e71dc235f974ab3c4f16b0
Nik <system512@ya.ru>
2025-09-25 10:03:51 +0300

    Fix error page

--- app/controllers/error/500.php
+++ app/controllers/error/500.php
@@ -9,5 +9,7 @@
 	$content['text']  = $error->getMessage(). ' in line '.$error->getLine();
 	$content['text']  .= '<br>'.$error->getFile();
 	//~ $APP->template->file($page['html'])->display($content);
-	$APP->template->file('error/500/500.htm')->display($content);
+
+	//themelink(null) нужен для того, что бы обнулить путь к шаблону, если ошибка возникла в коде шаблона
+	$APP->template->themelink(null)->file('error/500/500.htm')->display($content);
 
