d17362384350eca767c8526ede1d9d4ae0813952
Nik <system512@ya.ru>
2025-10-05 04:25:03 +0300

    Add function remove avatar

--- app/controllers/admin/options/users/save.php
+++ app/controllers/admin/options/users/save.php
@@ -16,17 +16,17 @@
 	//========================================================
 
 
-	$user['login'] 	= $_POST['login'];	unset($_POST['login']);
-	$user['name'] 	= $_POST['name'];	unset($_POST['name']);
-	$user['email'] 	= $_POST['email'];	unset($_POST['email']);
-	//$user['hash'] 	= $_POST['hash'];
-	$user['info'] 	= $_POST['info'];	unset($_POST['info']);
-	$user['disable']= $_POST['disable'];	unset($_POST['disable']);
-	$user['password']= $_POST['password'];	unset($_POST['password']);
+	$user['login']    = $_POST['login'];    unset($_POST['login']);
+	$user['name']     = $_POST['name'];     unset($_POST['name']);
+	$user['email'] 	  = $_POST['email'];    unset($_POST['email']);
+	$user['info']     = $_POST['info'];     unset($_POST['info']);
+	$user['disable']  = $_POST['disable'];  unset($_POST['disable']);
+	$user['password'] = $_POST['password']; unset($_POST['password']);
+	$user['logo']     = $_POST['logo'];     unset($_POST['logo']);
 
 
-
-
+	//Бланк имени файла аватарки
+	$avatarFile = $config['upload']['folder'].$user['login'].'.png';
 
 	//========================================================
 	//				РАЗБИРАЕМСЯ С ФАЙЛОМ АВАТАРКИ
@@ -34,47 +34,33 @@
 	if ($_FILES["avatar"]['name'] != '')
 	{
 
-		if ($_FILES["avatar"]["size"] > 1024*32*1024)
-		{
-			echo ("Размер файла превышает три  и два мегабайта");
-			exit;
-		}
+		if ($_FILES["avatar"]["size"] > 1024*32*1024) exit("Размер файла превышает 32 мегабайта");
 
 		// Проверяем загружен ли файл
-		if (!is_uploaded_file($_FILES["avatar"]["tmp_name"]))
-		{
-			echo("Ошибка загрузки файла");
-			exit;
-		}
+		if (!is_uploaded_file($_FILES["avatar"]["tmp_name"])) exit("Ошибка загрузки файла");
 
 		// Если файл загружен успешно, перемещаем его
 		// из временной директории в конечную
 		//$fin_file = "temp/upload/".$_FILES["avatar"]["name"];
 
-		$fin_file = $config['upload']['folder'].$user['login'].'.png';
+		if (!is_dir($config['upload']['folder'])) mkdir($config['upload']['folder'], 0775, true);
+
+		//~ $fin_file = $config['upload']['folder'].$user['login'].'.png';
 		//перемещяем в папку public
-		move_uploaded_file($_FILES["avatar"]["tmp_name"], $fin_file);
+		move_uploaded_file($_FILES["avatar"]["tmp_name"], $avatarFile);
 		//list($width, $height, $type, $attr) = getimagesize($fin_file);
 
-		$user['logo'] 	= $fin_file;
+		$user['logo'] = $avatarFile;
 	}
 
-
+	//Если нам требуется стереть файл аватарки
+	if (!$user['logo'] and is_readable($avatarFile)) unlink($avatarFile);
 
 
 	//========================================================
 	//				ПРАВА ПОЛЬЗОВАТЕЛЯ
 	//========================================================
 	$user['denied'] = $_POST['denied'];
-	//~ unset($user['access']);
-	//~ foreach ($_POST as $_name => $_value)
-	//~ {
-		//~ if ($_value)
-		//~ {
-			//~ $user['access'][base64_decode($_name)] = $_value;
-		//~ }
-	//~ }
-
 
 
 	try
