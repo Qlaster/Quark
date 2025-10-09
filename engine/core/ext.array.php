36a056aafa953f9fb08ca4b496f2d4f55f83ce6f
Nik <system512@ya.ru>
2025-10-10 00:36:59 +0300

    Fix warning

--- engine/core/ext.array.php
+++ engine/core/ext.array.php
@@ -3,15 +3,15 @@
 //TODO: DEPRICATED
 function array_keyding($array, $column)
 {
-	return array_combine( array_column((array) $array, $column), $array );
+	return array_combine( array_column((array) $array, $column), (array) $array );
 }
 
 function array_keycolumn($column, $array)
 {
-	return array_combine( array_column((array) $array, $column), $array );
+	return array_combine( array_column((array) $array, $column), (array) $array );
 }
 
 function array_key_column($column, $array)
 {
-	return array_combine( array_column((array) $array, $column), $array );
+	return array_combine( array_column((array) $array, $column), (array) $array );
 }
