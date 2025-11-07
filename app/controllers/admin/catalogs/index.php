3e451c91019da1be6263943313678cc6e896f499
Nik <system512@ya.ru>
2025-11-07 04:37:23 +0300

    add context menu for catalog

--- app/controllers/admin/catalogs/index.php
+++ app/controllers/admin/catalogs/index.php
@@ -23,6 +23,12 @@
 	}
 
 	$content['patterns']['list'] = $APP->catalog->patterns();
+	
+	$content['menu']['tools']['list'][1]['icon'] = "fa fa-wrench";
+	$content['menu']['tools']['list'][1]['button'][1]['head'] = "Конфигурация справочников";
+	$content['menu']['tools']['list'][1]['button'][1]['icon'] = "fa fa-cogs";
+	$content['menu']['tools']['list'][1]['button'][1]['link'] = "admin/tools/codeeditor/?config=".$_ENV['facades']['path'].'/catalog.ini';
+
 
 	$APP->template->file('admin/catalogs/list.html')->display($content);
 
