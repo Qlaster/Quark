d3083117beb13545d07dda50eb19f22cd420529c
Nik <system512@ya.ru>
2026-05-27 13:57:12 +0300

    [fix] ENV edit form

--- app/controllers/admin/options/env/index.php
+++ app/controllers/admin/options/env/index.php
@@ -9,14 +9,15 @@
 	$content['config']['body'] 		= is_readable($envFile) ? file_get_contents($envFile) : '';
 	$content['config']['action'] 	= "admin/options/env/save";
 	$content['config']['filename']	= $envFile;
-	$content['config']['title'] 	= 'Установить объекты';
+	$content['config']['title'] 	= 'Сохранить';
 
 	$content['title'] 				= $_GET['config'];
 
-	$content['menu']['config']['list']['.env']['head']     = 'ENV платформы';
-	$content['menu']['config']['list']['.env']['link']     = 'admin/options/env/?file=.env';
+
 	$content['menu']['config']['list']['app/.env']['head'] = 'ENV приложения';
 	$content['menu']['config']['list']['app/.env']['link'] = 'admin/options/env/?file=app/.env';
+	$content['menu']['config']['list']['.env']['head']     = 'ENV платформы';
+	$content['menu']['config']['list']['.env']['link']     = 'admin/options/env/?file=.env';
 	$content['menu']['config']['list'][$envFile]['active'] = 'active';
 
 	if ($content['title'])
